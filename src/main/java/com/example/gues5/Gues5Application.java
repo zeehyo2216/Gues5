e227e67e794df66642c19864ff47780a1d65a2c6
zeehyo <zeehyo2218@gmail.com>
2023-08-11 20:55:55 +0900

    Initial commit Init with Spring Initializr

---
+++ src/main/java/com/example/gues5/Gues5Application.java
@@ -0,0 +1,13 @@
+package com.example.gues5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gues5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gues5Application.class, args);
+    }
+
+}
