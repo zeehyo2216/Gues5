afbed3cf4237d627bc18853c6267721e632329b4
zeehyo <zeehyo2218@gmail.com>
2023-08-22 23:19:18 +0900

    Cacheable key값 구성

--- src/main/java/com/example/gues5/controller/GenerateWordController.java
+++ src/main/java/com/example/gues5/controller/GenerateWordController.java
@@ -1,7 +1,10 @@
 package com.example.gues5.controller;
 
+import com.example.gues5.domain.User;
 import com.example.gues5.service.GenerateWordService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GenerateWordController {
     GenerateWordService generateWordService = new GenerateWordService();
+
+
     @PostMapping("/answer")
     public void generateWord(){
-            generateWordService.generate();
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            User user = (User) authentication.getPrincipal();
+            Long userid = user.getId();
+            generateWordService.generate(userid);
     }
 }
--- src/main/java/com/example/gues5/service/GenerateWordService.java
+++ src/main/java/com/example/gues5/service/GenerateWordService.java
@@ -1,17 +1,26 @@
 package com.example.gues5.service;
 
+import com.example.gues5.domain.User;
 import com.example.gues5.dto.Answer;
+import com.example.gues5.dto.UserRegi;
 import com.example.gues5.dto.WebClientURI;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
 
 @Service
 public class GenerateWordService {
 
     //WebClient로 Random 한 단어를 가져온다.
     WebClientRequestService webClientRequestService = new WebClientRequestService();
+
     @Cacheable(cacheNames = "answer", key = "#userid")
-    public Answer generate(){
+    public Answer generate(Long userid){
+
         Answer answer = new Answer();
         WebClientURI webClientURI = new WebClientURI(
                 "https://random-word-api.vercel.app/",
